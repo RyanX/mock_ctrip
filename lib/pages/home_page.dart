8bdf4228e91a1f607102bcc038a8d0e7e4f328ac
ryanxr <ryanxr@outlook.com>
2022-05-13 19:42:22 +0800

    feature#增加轮播图

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,16 +7,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final PageController _controller = PageController(
     initialPage: 0,
   );
+
+  List _images = [
+    'https://dimg04.c-ctrip.com/images/0zg1q120009ggsv8s38DA.jpg',
+    'https://dimg04.c-ctrip.com/images/0zg1p120009ggswfhA338.jpg',
+    'https://dimg04.c-ctrip.com/images/0zg43120009ggsk3n380D.jpg'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              child: Swiper(
+                itemCount: _images.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _images[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
