6dcc9ac139652bc19e48a1bcca0ce6f8146cc0db
ryanxr <ryanxr@outlook.com>
2022-05-13 20:09:19 +0800

    feature#增加appBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
+const APPBAR_SCROLL_OFFSET = 100;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,34 +13,84 @@ class _HomePageState extends State<HomePage> {
     initialPage: 0,
   );
 
-  List _images = [
+  final List _images = [
     'https://dimg04.c-ctrip.com/images/0zg1q120009ggsv8s38DA.jpg',
     'https://dimg04.c-ctrip.com/images/0zg1p120009ggswfhA338.jpg',
     'https://dimg04.c-ctrip.com/images/0zg43120009ggsk3n380D.jpg'
   ];
 
+  double appBarAlpha = 0;
+
+  void _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              child: Swiper(
-                itemCount: _images.length,
-                autoplay: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _images[index],
-                    fit: BoxFit.fill,
-                  );
-                },
-                pagination: SwiperPagination(),
+      body: Stack(
+        children: [
+          MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  // 滚动列表监听且是组件内的第一个元素时
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+                return true;
+              },
+              child: ListView(
+                children: [
+                  Container(
+                    height: 200,
+                    child: Swiper(
+                      itemCount: _images.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _images[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('aaaa'),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
