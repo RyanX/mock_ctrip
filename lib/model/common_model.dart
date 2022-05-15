369fc8785b560df713f07efa17f5942658b5a624
ryanxr <ryanxr@outlook.com>
2022-05-15 14:33:21 +0800

    feature#增加model

---
+++ lib/model/common_model.dart
@@ -0,0 +1,19 @@
+class CommonModel {
+  final String? icon;
+  final String? title;
+  final String? url;
+  final String? statesBarColor;
+  final bool? hideAppBar;
+
+  CommonModel(
+      {this.icon, this.title, this.url, this.statesBarColor, this.hideAppBar});
+
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
+    return CommonModel(
+        icon: json['icon'],
+        title: json['title'],
+        url: json['url'],
+        statesBarColor: json['statesBarColor'],
+        hideAppBar: json['hideAppBar']);
+  }
+}
