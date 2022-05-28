9d96368afc5bb42d4bcb48badae113539de2d651
ryan <ryanxr@outlook.com>
2022-05-28 15:34:23 +0800

    feature#旅拍功能完善

--- lib/pages/travel_tab_page.dart
+++ lib/pages/travel_tab_page.dart
@@ -5,6 +5,7 @@ import 'package:mock_ctrip/model/travel_tab_model.dart';
 
 import '../dao/travel_tab_dao.dart';
 import '../model/travel_model.dart';
+import '../widget/webview.dart';
 
 const _TRAVEL_URL =
     'https://m.ctrip.com/restapi/soa2/16189/json/searchTripShootListForHomePageV2?_fxpcqlniredt=09031014111431397988&__gw_appid=99999999&__gw_ver=1.0&__gw_from=10650013707&__gw_platform=H5';
@@ -22,9 +23,13 @@ class TravelTabPage extends StatefulWidget {
   _TravelTabPageState createState() => _TravelTabPageState();
 }
 
-class _TravelTabPageState extends State<TravelTabPage> {
+class _TravelTabPageState extends State<TravelTabPage>
+    with AutomaticKeepAliveClientMixin {
   List<TravelItem>? travelItems;
   int pageIndex = 1;
+
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
@@ -44,7 +49,7 @@ class _TravelTabPageState extends State<TravelTabPage> {
       crossAxisCount: 2,
       itemCount: travelItems?.length ?? 0,
       itemBuilder: (context, index) {
-        return _TravelItems(index: index, travelItems: travelItems?[index]);
+        return _TravelItems(index: index, item: travelItems?[index]);
       },
     ));
   }
@@ -80,23 +85,140 @@ class _TravelTabPageState extends State<TravelTabPage> {
     return filterItems;
   }
 
-  Widget _TravelItems({int? index, TravelItem? travelItems}) {
+  Widget _TravelItems({int? index, TravelItem? item}) {
+    return GestureDetector(
+      onTap: () {
+        if (item != null &&
+            item.article != null &&
+            item.article!.urls != null &&
+            item.article!.urls!.isNotEmpty) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Webview(
+                        url: item.article?.urls?[0].h5Url,
+                        title: '详情',
+                      )));
+        }
+      },
+      child: Card(
+        child: PhysicalModel(
+          color: Colors.transparent,
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.circular(5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _itemImage(item),
+              Container(
+                padding: const EdgeInsets.all(4),
+                child: Text(
+                  item?.article?.articleTitle ?? '',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontSize: 14, color: Colors.black87),
+                ),
+              ),
+              _infoText(item)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _itemImage(TravelItem? item) {
+    return Stack(
+      children: [
+        Image.network(item?.article?.images?[0].dynamicUrl ?? ''),
+        Positioned(
+            bottom: 8,
+            left: 8,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(5, 1, 5, 1),
+              decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 3),
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                      size: 12,
+                    ),
+                  ),
+                  LimitedBox(
+                    maxWidth: 130,
+                    child: Text(
+                      _poiName(item),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  )
+                ],
+              ),
+            ))
+      ],
+    );
+  }
+
+  String _poiName(TravelItem? item) {
+    return (item?.article?.pois ?? []).isEmpty
+        ? '未知'
+        : item?.article?.pois?[0].poiName ?? '未知';
+  }
+
+  _infoText(TravelItem? item) {
     return Container(
-      child: Text('$index'),
+      padding: const EdgeInsets.fromLTRB(6, 0, 6, 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              PhysicalModel(
+                color: Colors.transparent,
+                clipBehavior: Clip.antiAlias,
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  item?.article?.author?.coverImage?.dynamicUrl ?? '',
+                  width: 24,
+                  height: 24,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                width: 90,
+                child: Text(
+                  item?.article?.author?.nickName ?? '',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontSize: 12),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              const Icon(
+                Icons.thumb_up,
+                size: 14,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 3),
+                child: Text(
+                  item?.article?.likeCount?.toString() ?? '',
+                  style: const TextStyle(fontSize: 10),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
-
-// class _TravelItem extends StatelessWidget {
-//   final TravelItem? item;
-//   final int? index;
-//
-//   const _TravelItem({Key? key, this.item, this.index}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Text('$index'),
-//     );
-//   }
-// }
